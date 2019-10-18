c6b53e365c8a354a3f89db2f5bb5c364115a188c
dinaminahi <46566190+dinaminahi@users.noreply.github.com>
2019-10-18 19:58:59 +0300

    Create day-05.js

---
+++ day-05.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/is-this-my-tail/train/javascript
+
+function correctTail(body, tail) {
+  const sub = body.substr(body.length - tail.length);
+  if (sub === tail) {
+     return true;
+  } else {
+    return false;
+  }
+} 
+  
